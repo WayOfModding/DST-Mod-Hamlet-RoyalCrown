a048170d4c842a88f6fbc1afa2479426f0747333
KaiserKatze <donizyo@gmail.com>
2019-01-30 20:27:13 +0800

    First blood

---
+++ modinfo.lua
@@ -0,0 +1,30 @@
+--The name of the mod displayed in the 'mods' screen.
+name = "[Hamlet] Royal Crown"
+
+--A description of the mod.
+description = "Royal Crown from Hamlet DLC of Don't Starve."
+
+--Who wrote this awesome mod?
+author = "KaiserKatze"
+
+--A version number so you can ask people if they are running an old version of your mod.
+version = "1.0.0"
+
+--This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
+api_version = 10
+
+dst_compatible = true
+
+--This lets clients know if they need to get the mod from the Steam Workshop to join the game
+all_clients_require_mod = true
+
+--This determines whether it causes a server to be marked as modded (and shows in the mod list)
+client_only_mod = false
+
+--This lets people search for servers with this mod by these tags
+server_filter_tags = {"hamlet"}
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+forumthread = ""
---
+++ modmain.lua
@@ -0,0 +1,34 @@
+local _G = GLOBAL
+local require = _G.require
+
+local DEBUG = false
+
+Assets =
+{
+}
+
+PrefabFiles =
+{
+}
+
+require "modstrings"
+
+--------------
+
+if DEBUG then
+  local SpawnPrefab = _G.SpawnPrefab
+
+  local function giveitem(item_name)
+    local item = SpawnPrefab(item_name)
+    if item then
+      inst.components.inventory:GiveItem(item)
+    end
+  end
+
+  AddPlayerPostInit(function(inst)
+    -- Spawn items in tester's inventory
+    if inst.components.inventory then
+      --giveitem("")
+    end
+  end)
+end
