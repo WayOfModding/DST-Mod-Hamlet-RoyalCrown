a18d4e87c5f9b8feea253a1d256f41940fb7f6e6
KaiserKatze <donizyo@gmail.com>
2019-01-31 00:24:30 +0800

    Update

---
+++ scripts/modrecipes.lua
@@ -0,0 +1,16 @@
+Recipe(
+  "pigcrownhat",
+  {
+    Ingredient("redgem", 4),
+    Ingredient("goldnugget", 8),
+    Ingredient("manrabbit_tail", 16),
+  },
+  RECIPETABS.DRESS,
+  TECH.SCIENCE_TWO,
+  nil,
+  nil,
+  nil,
+  nil,
+  nil,
+  "images/pigcrownhat.xml" -- atlas
+)
