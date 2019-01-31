ce2e079fe239f22fa3acb09692e651e943286e86
KaiserKatze <donizyo@gmail.com>
2019-01-31 14:51:59 +0800

    Update

--- scripts/prefabs/pigcrownhat.lua
+++
@@ -1,7 +0,0 @@
-local MakeHat = require("modhats")
-
-return MakeHat("pigcrown", function(inst)
-  inst.components.equippable.dapperness = TUNING.DAPPERNESS_SUPERHUGE
-  inst:AddTag("pigcrown")
-  return inst
-end)
